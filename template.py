d9a7ec3c929b32935e8b71fe9f68d0d025fb4e73
kumarshivam12 <kumarshivam192@gmail.com>
2020-09-13 13:03:04 +0530

    Added CP Template

---
+++ template.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from itertools import accumulate
+from collections import Counter
+import imp
+import re
+import sys
+
+sys.setrecursionlimit(10**8)
+def ii(): return int(sys.stdin.readline())
+def si(): return sys.stdin.readline()
+def li(): return list(map(int, sys.stdin.readline().split()))
+def mi(): return map(int, sys.stdin.readline().split())
+def li2(N): return [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+'''
+uses :
+
+d = defaultdict(int)
+d[key] += value
+a = Counter(A).most_common()
+list(accumulate(A))
+
+'''
